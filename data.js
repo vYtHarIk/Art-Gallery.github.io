2ce4377add77bb2bbcf2484961aebe5f6cd38709
Hoho <giopagolif25@gmail.com>
2026-05-16 00:57:08 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 export const paintings = [
-  { id: 1, title: "CASCATE DI TIVOLI", year: 1761, artist: "Giovanni Battista Piranesi", location: "Tivoli", image: "/img/image1.png" },
-  { id: 2, title: "PORTRAIT OF VINCENT VAN GOGH", year: 1886, artist: "Vincent van Gogh", location: "Paris", image: "/img/image2.png" },
-  { id: 3, title: "UNEQUAL MARRIAGE", year: 1862, artist: "Vasily Pukirev", location: "Moscow", image: "/img/image3.png" },
-  { id: 4, title: "THE HAPPY VIOLINIST", year: 1624, artist: "Gerard van Honthorst", location: "Amsterdam", image: "/img/image4.png" },
-  { id: 5, title: "THE ARCADIAN", year: 1834, artist: "Thomas Cole", location: "New York", image: "/img/image5.png" },
-  { id: 6, title: "GOLFO DI NAPOLI", year: 1845, artist: "Anton Sminck van Pitloo", location: "Naples", image: "/img/image6.png" },
+  { id: 1, title: "CASCATE DI TIVOLI", year: 1761, artist: "Giovanni Battista Piranesi", location: "Tivoli", image: "/Art-Gallery.github.io/img/image1.png" },
+  { id: 2, title: "PORTRAIT OF VINCENT VAN GOGH", year: 1886, artist: "Vincent van Gogh", location: "Paris", image: "/Art-Gallery.github.io/img/image2.png" },
+  { id: 3, title: "UNEQUAL MARRIAGE", year: 1862, artist: "Vasily Pukirev", location: "Moscow", image: "/Art-Gallery.github.io/img/image3.png" },
+  { id: 4, title: "THE HAPPY VIOLINIST", year: 1624, artist: "Gerard van Honthorst", location: "Amsterdam", image: "/Art-Gallery.github.io/img/image4.png" },
+  { id: 5, title: "THE ARCADIAN", year: 1834, artist: "Thomas Cole", location: "New York", image: "/Art-Gallery.github.io/img/image5.png" },
+  { id: 6, title: "GOLFO DI NAPOLI", year: 1845, artist: "Anton Sminck van Pitloo", location: "Naples", image: "/Art-Gallery.github.io/img/image6.png" },
 ];
 
 export const allArtists = [...new Set(paintings.map(p => p.artist))];
